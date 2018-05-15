60e183d866fa005c659d7c1a8d6b7f8ebc17e32c
armantark <armantarkhanian@berkeley.edu>
2018-05-15 11:04:18 -0700

    finished

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ STAGES = ["┏━━┓\n┃\n┃\n┃\n┻━\n",
 
 def openingmenu():
     answer = 0
-    while answer != 1 or answer != 2:
+    while answer != '1' and answer != '2':
         answer = input("Welcome to Hangman!\n"
                        "[1] random word\n"
                        "[2] custom word\n")
-    if answer == 1:
+    if answer == '1':
         randomizeWord()
-    elif answer == 2:
+    elif answer == '2':
         customizeWord()
 
 
@@ -45,19 +45,41 @@ def game():
     win = False
     currstage = 0
     currguess = ""
+    currletter = ""
     for _ in range(len(guessingword)):
-        currguess.join("_")
+        currguess += "_"
+    currguess += "\n"
+    usedletters = set([])
     while not win:
         print(STAGES[currstage])
+        print(currguess)
+        print(list(reversed(list(usedletters))))
+        while len(currletter) != 1 or not currletter.isalpha():
+            currletter = input("What is your letter to guess?\n")
+        usedletters.add(currletter)
+        i = 0
+        indices = []
+        for letter in guessingword:
+            if letter == currletter:
+                indices.append(i)
+            i += 1
+        if len(indices) == 0:
+            print("Strike!\n")
+            currstage += 1
+            if currstage >= 5:
+                print("Game over\nThe word was " + guessingword + "\n")
+                break
+        for index in indices:
+            liststr = list(currguess)
+            liststr[index] = currletter
+            currguess = ''.join(liststr)
+        currletter = ""
+        if "_" not in currguess:
+            win = True
+            print("You won! The word was " + guessingword + "\n")
 
-        currstage += 1
-        if currstage > 5:
-            print("Game over\n")
-            break
 
-
-
-while quitGame != 'y':
+while quitGame != 'n':
     openingmenu()
     game()
     quitGame = input("Play again? [y] or [n]\n")
